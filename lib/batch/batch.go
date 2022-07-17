8159e0a2c267e27ae0af7d3f0770f74a6f21f6e2
Samandar <samandar0311@gmail.com>
2022-07-18 04:15:05 +0500

    commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -10,9 +12,62 @@ type user struct {
 
 func getOne(id int64) user {
 	time.Sleep(time.Millisecond * 100)
-	return user{ID: id}
+	return user{
+		ID: id,
+	}
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	var wg sync.WaitGroup
+
+	res = make([]user, 0, n)
+	workSize := n / pool
+
+	counter := SafeCounter{v: make(map[string]int)}
+	counter.v["userId"] = 0
+
+	for i := 1; i <= int(pool); i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			for j := 1; j <= int(workSize); j++ {
+
+				if counter.Value("userId") == int(n) {
+					return
+				}
+				res = append(res, getOne(int64(counter.GetNewUserId("userId"))))
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	return
+}
+
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+func (c *SafeCounter) GetNewUserId(key string) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	temp := c.v[key]
+	c.v[key]++
+	return temp
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mu.Lock()
+	fmt.Println(c.v[key])
+	c.v[key]++
+	c.mu.Unlock()
+}
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.v[key]
 }
